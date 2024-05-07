940d77602dd3ad884fd8b2160b826a200799b0f3
Lio <lionooil888@gmail.com>
2024-05-07 00:48:56 -0400

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import { getBlogs } from "@/../libs/client";
 
 export default async function StaticPage() {
@@ -10,7 +11,19 @@ export default async function StaticPage() {
 
     return (
       <>
-       <div className="h-screen container mx-auto mt-10">
+      <h2 className="text-4xl font-bold mb-4 mt-8 text-center">WELCOME TO LIOSS PORTFOLIO!!</h2>
+      <Image
+        src="/header-icon.svg"
+        alt="Vercel Logo"
+        className="dark:invert"
+        width={500}
+        height={24}
+        style={{
+  margin: "auto",
+}}
+        priority
+      />
+      <div className="h-screen container mx-auto mt-10">
             <ul className="space-y-10">
                 {contents.map((blog) => (
                  <li key={blog.id} className="p-5 rounded shadow-lg content flex justify-between items-center">
@@ -26,6 +39,10 @@ export default async function StaticPage() {
                 ))}
             </ul>
         </div>
+        <div className="h-screen container mx-auto mt-10">
+        <h2>LINKS</h2>
+
+</div>
       </>
     );
   }
