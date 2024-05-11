55eeef42a3f3d0cee8b1c0af1c31f16f908fab3b
Lio <lionooil888@gmail.com>
2024-05-10 23:49:40 -0400

    20240510 commit

--- components/WorksMotionS.tsx
+++ components/WorksMotionS.tsx
@@ -5,44 +5,50 @@ import Link from "next/link";
 
 const WorksMotionS = () => {
   return (
-    <motion.div initial={{ opacity: 0, y: 100 }} whileInView={{ opacity: 1, y: 0 }}  transition={{ duration: 0.4, delay: 0.5 }} viewport={{ once: true }}>
+    <motion.div initial={{ opacity: 0, y: 100 }} whileInView={{ opacity: 1, y: 0 }}  transition={{ duration: 0.5, delay: 0.5 }} viewport={{ once: true }}>
       <div className="flex place-content-around">
       <Link href="/">
+      <div className="relative group/item overflow-hidden rounded-lg shadow">
+      <p className="absolute transition duration-500 opacity-0 group-hover/item:opacity-100 group-hover/item:bg-gray-400/50 size-full z-10 text-center">エンジニア向け技術サイトHTML/CSS/JavaScript/PHP/MySQL</p>
       <Image
         src="/thumbnail/sci-quotes-bot.jpg"
         alt="Header icon"
-        className="dark:invert shadow"
+        className="dark:invert group-hover/item:scale-125 ease-out duration-500"
         width={400}
         height={24}
         style={{
           margin: "auto"
 }}
         priority
-      /></Link>
+      /></div></Link>
       <Link href="/">
+      <div className="relative group/item overflow-hidden rounded-lg shadow">
+      <p className="absolute transition duration-500 opacity-0 group-hover/item:opacity-100 group-hover/item:bg-gray-400/50 size-full z-10 text-center">エンジニア向け技術サイトHTML/CSS/JavaScript/PHP/MySQL</p>
       <Image
         src="/thumbnail/tonkatsu.jpg"
         alt="Header icon"
-        className="dark:invert shadow"
+        className="dark:invert group-hover/item:scale-125 ease-out duration-500"
         width={400}
         height={24}
         style={{
           margin: "auto"
 }}
         priority
-      /></Link>
+      /></div></Link>
       <Link href="/">
+      <div className="relative group/item overflow-hidden rounded-lg shadow">
+      <p className="absolute transition duration-500 opacity-0 group-hover/item:opacity-100 group-hover/item:bg-gray-400/50 size-full z-10 text-center">エンジニア向け技術サイトHTML/CSS/JavaScript/PHP/MySQL</p>
       <Image
         src="/thumbnail/yuri-interview.jpg"
         alt="Header icon"
-        className="dark:invert shadow"
+        className="dark:invert group-hover/item:scale-125 ease-out duration-500"
         width={400}
         height={24}
         style={{
           margin: "auto"
 }}
         priority
-      /></Link>
+      /></div></Link>
       </div>
 
       </motion.div>
